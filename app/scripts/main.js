2c3f31faec6b9301ca3d48ab7b774e446ff39968
TJ <tonyjsouthern@gmail.com>
2018-06-29 18:17:41 -0400

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,80 @@
+let value;
+
+let results = {
+  site: '',
+  actOn: '',
+  clickDimensions: '',
+  google: '',
+  marketo: '',
+  pardot: '',
+  sf: ''
+}
+
+$('.submit').on("click", function () {
+  value = $('.input').val();
+  scriptCheck(value)
+})
+
+$('form').submit(function (evt) {
+   evt.preventDefault(); //prevents the default action
+});
+
+function scriptCheck (url) {
+  axios.post('https://powerful-island-56445.herokuapp.com/single-domain/', {
+  domain: url
+})
+.then(function (response) {
+  return responseHandler(response);
+  })
+}
+
+function responseHandler (response) {
+  var data                = response.data;
+  results.site            = data.site;
+  results.actOn           = data.actOn;
+  results.clickDimensions = data.clickDimensions;
+  results.google          = data.google;
+  results.marketo         = data.marketo;
+  results.pardot          = data.pardot;
+  results.sf              = data.sf;
+  $('.results-cont').removeClass("hidden")
+  $('.results-cont').html(htmlTemplate())
+  checkTf();
+}
+
+function checkTf () {
+ $('.tf').each(function (){
+   if($(this).text() == 'true') {
+     $(this).addClass('true')
+   }else{
+     $(this).addClass('false')
+   }
+ })
+}
+
+function htmlTemplate () {
+  return `
+    <div class="results">
+    <p class="title is-4">Website: ${results.site}</p>
+    <div class="companies">
+      <p class="margin-bot"><span class="is-bold">Act-On:</span></p>
+      <p class="margin-bot"><span class="is-bold">Click Dimensions: </span></p>
+      <p class="margin-bot"><span class="is-bold">Google:</span></p>
+      <p class="margin-bot"><span class="is-bold">Marketo:</span></p>
+      <p class="margin-bot"><span class="is-bold">Pardot:</span></p>
+      <p class="margin-bot"><span class="is-bold">Salesfusion:</span></p>
+    </div>
+
+    <div class="values-cont">
+      <p class="tf">${results.actOn}</p>
+      <p class="tf">${results.clickDimensions}</p>
+      <p class="tf">${results.google}</p>
+      <p class="tf">${results.marketo}</p>
+      <p class="tf">${results.pardot}</p>
+      <p class="tf">${results.sf}</p>
+    </div>
+
+    </div>
+  `
+}
+// http://iheartcamo.com
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Scraper FE
+
+This is a simple front-end for the scraper back-end.
+
+It allows a user to input a web-address and runs a check to see if tracking scripts from any of the following companies are present:  
+
+* Marketo
+* ActOn
+* Salesfusion
+* Google
+* ClickDimensions
+* Pardot
+
+The live URL for this site can be found at:  
+https://powerful-island-56445.herokuapp.com/
+
+## Improvements
+I will be adding Hubspot to the list of tracking scripts that will be checked. 
